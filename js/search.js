1a6e60cdf778f38f6a19a00b6acdd95c1523ef18
KalvinSon <git@github.com:KalvinSon/Travel-planner.git>
2022-04-25 13:41:00 +0930

    implemented starter code in search.js

--- js/search.js
+++ js/search.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-    
+    var windowURL = window.location.search;
+    const urlParams = new URLSearchParams(windowURL);
+    alert(urlParams);
+    var country = urlParams.get("countryCode");
+    var city = urlParams.get("city");
+    var startFrom = urlParams.get("priceFrom");
+    var endFrom = urlParams.get("priceTo");
+    var dateFrom = urlParams.get("startDateTime");
+    var dateTo = urlParams.get("endDateTime");
+    console.log(country);
+    console.log(city);
+    console.log(startFrom);
+    console.log(endFrom);
+    console.log(dateFrom);
+    console.log(dateTo);
+
+    $("#search-button").click(function (e){
+        e.stopPropagation();
+        e.preventDefault();
+        window.location.href = "./search.html?";
+    });
 });
